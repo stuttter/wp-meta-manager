a91c734eb00d0431671f6fad11fe09c823f6a927
Pippin Williamson <pippin@pippinsplugins.com>
2017-02-07 15:50:18 -0700

    Add missing meta manager file

--- wp-meta-manager/includes/admin.php
+++ wp-meta-manager/includes/admin.php
@@ -7,3 +7,23 @@
 
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
+
+/**
+ * Adds the Metadata menu under Tools
+ *
+ * @since 1.0
+ */
+function wp_meta_manager_admin_menu() {
+
+	$hook = add_management_page( __( 'Meta Manager', 'wp-meta-manager' ), __( 'Meta Manager', 'wp-meta-manager' ), 'manage_options', 'wp-meta-manager', 'wp_meta_manager_admin' );
+
+}
+
+/**
+ * Render Meta Manager admin
+ *
+ * @since 1.0
+ */
+function wp_meta_manager_admin() {
+	echo 'test';
+}
