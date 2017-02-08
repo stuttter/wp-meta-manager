9c3a7e905af3cf1f08e8aa9a1bc2a32d7e81146a
Pippin Williamson <pippin@pippinsplugins.com>
2017-02-07 23:03:01 -0700

    Introduce Add New Meta UI / function

--- wp-meta-manager/includes/hooks.php
+++ wp-meta-manager/includes/hooks.php
@@ -14,6 +14,9 @@ add_action( 'admin_menu', 'wp_meta_manager_admin_menu' );
 // Register meta tables
 add_action( 'admin_init', '_wp_register_meta_types', 0 );
 
-// Ajax views
+// Add new meta handling
+add_action( 'admin_init', 'wp_meta_process_add_meta' );
+
+// Ajax processing
 add_action( 'wp_ajax_edit_meta', 'wp_meta_ajax_edit_response' );
 add_action( 'wp_ajax_delete_meta', 'wp_meta_ajax_delete_response' );
